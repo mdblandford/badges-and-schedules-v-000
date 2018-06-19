93485b8830531b14fe6df5d6fa822ecad361d94d
Michael Blandford <mdblandford@gmail.com>
2018-06-19 20:16:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,8 @@ def assign_rooms(array)
   room_assignments = []
   array.each_with_index do |speaker, index|
     room_assignments << "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
+  end
+  room_assignments
 end
 
 def printer(array)
